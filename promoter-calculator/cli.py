0126ae43c1ff00d813a32256aacd79329cb7894d
Cameron Roots <38054423+croots@users.noreply.github.com>
2023-02-14 18:23:49 -0600

    More CLI Options

--- promoter-calculator/cli.py
+++ promoter-calculator/cli.py
@@ -4,7 +4,10 @@ Command line interface for the promoter-calculator.
 
 import argparse
 import os
+import csv
+from copy import copy
 from .wrapper import promoter_calculator
+from dataclasses import asdict
 
 def print_promo_calculator(output):
     print("#------------------------------------------------------#")
@@ -34,34 +37,77 @@ def main():
         help="Input DNA/RNA sequence.",
     )
 
+    parser.add_argument(
+        '-v', '--verbosity',
+        action='store',
+        metavar='int',
+        dest='v',
+        required=False,
+        type=int,
+        help="Verbosity level. 0 = silent, 1 = progress bar, 2 = detailed progress.",
+        default=1
+    )
+
+    parser.add_argument(
+        '-o', '--output',
+        action='store',
+        metavar='filepath',
+        dest='o',
+        required=False,
+        type=str,
+        help="Output file path.",
+    )
+
+    parser.add_argument(
+        '-j', '--threads',
+        action='store',
+        metavar='int',
+        dest='j',
+        required=False,
+        type=int,
+        help="Number of threads to use. (Min 1)",
+        default=1
+    )
+
     # @TODO: Add option to specify output file
     # @TODO: Add option to run quietly
 
     options = parser.parse_args()
 
-    # Show help if no arguments are given 
+    # Show help if no input is provided
     if not options.i:
         parser.print_help()
         return
 
-
-    if options.i is None:
-        print("No input provided. Exiting.")
+    # Show help if threads is invalid
+    if options.j < 1:
+        parser.print_help()
         return
+
     if os.path.isfile(options.i):
         print("Input file is a file. Reading sequence from file.")
         with open(options.i, 'r') as f:
             sequence_lines = f.readlines()
+            sequence = []
             for line in sequence_lines:
                 line = line.strip()
-            sequence = "".join(f.readlines())
-            print(sequence)
+                sequence.append(line)
+            sequence = "".join(sequence)
+            print(f"Sequence length: {len(sequence)}")
     else:
         sequence = options.i
 
-    
-    output = promoter_calculator(sequence)
-    print_promo_calculator(output)
+    output = promoter_calculator(sequence, verbosity = options.v, threads = options.j)
+
+    if not options.o:
+        print_promo_calculator(output)
+    else:
+        # Make a CSV with all the hits
+        with open(options.o, 'w') as f:
+            writer = csv.writer(f)
+            writer.writerow(asdict(output[0]).keys())
+            for row in output:
+                writer.writerow(asdict(row).values())
 
 if __name__ == "__main__":
     main()
