8a62b9b95e88c1bd6c2f77412158050b7d8673a7
Cameron Roots <38054423+croots@users.noreply.github.com>
2023-03-17 11:13:14 -0500

    Version increment to 1.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="promotercalculator",
-    version="1.1",
+    version="1.2",
     packages=["promoter_calculator"],
     package_dir={'promoter_calculator': 'promoter-calculator'},
 
