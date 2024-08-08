0f3781218dd147bb4291c922f0b513bd82c378ef
croots <38054423+croots@users.noreply.github.com>
2024-08-08 14:31:52 -0500

    Branding, dependency fix

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name="promotercalculator",
-    version="1.2.2",
+    version="1.2.3",
     packages=["promoter_calculator"],
     package_dir={'promoter_calculator': 'promoter-calculator'},
 
     # Project uses reStructuredText, so ensure that the docutils get
     # installed or upgraded on the target machine
-    install_requires=['Bio', 'numpy', 'scikit_learn', 'scipy'],
+    install_requires=['biopython', 'numpy', 'scikit_learn', 'scipy'],
 
     # metadata to display on PyPI
     author="Cameron Roots",
